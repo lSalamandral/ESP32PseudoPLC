83511608e8252e2b268eeba49325d60a3ea94d22
Salamandra <pablicicu6@gmail.com>
2022-08-29 22:58:03 -0300

    Agrege como estas

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ std::list<TaskHandle_t> tareas;
 TaskHandle_t xHandle=NULL;
 bool AND(int a,int b,int d,bool c){
 bool aux;
-switch (b) //hola
+switch (b) //hola como estas
 {
 case 0:
   aux=digitalRead(13);
